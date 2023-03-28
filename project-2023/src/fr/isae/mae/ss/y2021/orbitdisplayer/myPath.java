8761c9a04ecb4cc70512c6f53a77877341576a66
Martí Prat i Esquerd <marti.prat-esquerdo@student.isae-supaero.fr>
2023-03-28 20:56:41 +0200

    Add comments.

--- project-2023/src/fr/isae/mae/ss/y2021/orbitdisplayer/myPath.java
+++ project-2023/src/fr/isae/mae/ss/y2021/orbitdisplayer/myPath.java
@@ -29,35 +29,49 @@ public class myPath extends ApplicationTemplate {
 
         private static ArrayList<GeodeticPoint> listOfStates;
 
+        /**
+
+         Displays a path with specified attributes and enables shape dragging.
+
+         Adds a marker layer with a single marker at position (90, 0).
+         */
         public void showtime() {
 
             // Add a dragger to enable shape dragging
             getWwd().addSelectListener(new BasicDragger(getWwd()));
 
+            // Create a new renderable layer
             RenderableLayer layer = new RenderableLayer();
 
             // Create and set an attribute bundle.
+            // Set the outline material to yellow and outline width to 2d.
             ShapeAttributes attrs = new BasicShapeAttributes();
             attrs.setOutlineMaterial(new Material(Color.YELLOW));
             attrs.setOutlineWidth(2d);
 
-            // Create a path, set some of its properties and set its attributes.
+            // Create a path using a list of positions converted from geo coordinates
             ArrayList<Position> pathPositions = geo2pos(listOfStates);
             Path path = new Path(pathPositions);
+
+            // Set the path's attributes
             path.setAttributes(attrs);
             path.setVisible(true);
             path.setAltitudeMode(WorldWind.RELATIVE_TO_GROUND);
             path.setPathType(AVKey.GREAT_CIRCLE);
+
+            // Add the path to the renderable layer
             layer.addRenderable(path);
 
-
-            // Add the layer to the model.
-            insertBeforeCompass(getWwd(), layer);
-
+            // Create a list of markers and add a single marker at position (90, 0)
             List<Marker> markers = new ArrayList<>(1);
             markers.add(new BasicMarker(Position.fromDegrees(90, 0), new BasicMarkerAttributes()));
+
+            // Create a marker layer and set its markers to the list of markers
             MarkerLayer markerLayer = new MarkerLayer();
             markerLayer.setMarkers(markers);
+
+            // Insert the marker layer and the renderable layer before the compass in the WorldWind window
+            insertBeforeCompass(getWwd(), layer);
             insertBeforeCompass(getWwd(), markerLayer);
         }
 
